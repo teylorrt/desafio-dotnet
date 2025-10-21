bc5ae6252f4321f4db736c4611316d572803b705
Teylor Tavares <teylorrt@gmail.com>
2025-10-20 23:21:22 -0300

    Initial docker setup

--- install.sh
+++ install.sh
@@ -19,3 +19,7 @@ node -v # Should print "v22.20.0".
 
 # Verify npm version:
 npm -v # Should print "10.9.3".
+
+### Docker ###
+#curl -fsSL https://get.docker.com -o get-docker.sh && \
+#    sh get-docker.sh
